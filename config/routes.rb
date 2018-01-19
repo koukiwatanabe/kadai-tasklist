a004015da10bec55709055219521366d365b7777
Kouki Watanabe <uguis.kouki@gmail.com>
2018-01-19 12:35:32 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  # 最初に飛ばされるところ
   root to: 'toppages#index'
+  
+  # ログイン用のルーティング
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  # ユーザ登録用のルーティング
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
   #root to: 'tasks#index'
-  
   resources :tasks
 end
